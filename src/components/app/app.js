2dc5bf42b4356c47c18b21ba22ad7eeaea49a045
Valery <nesterovsky.valery@gmail.com>
2025-05-10 20:36:07 +0300

    Practicing props on the project

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,31 +1,54 @@
+import { Component } from 'react';
+
 import AppInfo from '../app-info/app-info';
 import AppFilter from '../app-filter/app-filter';
 import SearchPanel from '../search-panel/search-panel';
 import EmployeesList from '../employees-list/employees-list';
 import EmployeesAddForm from '../employees-add-form/employees-add-form';
+import WhoAmI from '../practice';
 import './app.css';
 
-function App() {
-  const data = [
-    {name: 'Will', salary: 2000, increase: true, id: 1},
-    {name: 'Jane', salary: 1000, increase: false, id: 2},
-    {name: 'Nik', salary: 4000, increase: false, id: 3},
-    {name: 'Alex', salary: 1200, increase: true, id: 4},
-  ];
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        { name: 'Will', salary: 2000, increase: true, id: 1 },
+        { name: 'Jane', salary: 1000, increase: false, id: 2 },
+        { name: 'Nik', salary: 4000, increase: false, id: 3 },
+        { name: 'Alex', salary: 1200, increase: true, id: 4 },
+      ]
+    }
+  }
 
-  return (
-    <div className="app">
-      <AppInfo />
+  deleteItem = (id) => {
+    this.setState(({ data }) => {
+      const index = data.findIndex(elem => elem.id === id);
 
-      <div className="search-panel">
-        <SearchPanel />
-        <AppFilter />
+      return {
+        data: data.filter(item => item.id !== id)
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div className="app">
+        {/* <AppInfo />
+
+        <div className="search-panel">
+          <SearchPanel />
+          <AppFilter />
+        </div>
+
+        <EmployeesList
+          data={this.state.data}
+          onDelete={this.deleteItem} />
+        <EmployeesAddForm /> */}
+        <WhoAmI name='Valera' surname='Nesterovsky' email='nsva2728' />
       </div>
-
-      <EmployeesList data={data} />
-      <EmployeesAddForm />
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/components/practice.js
+++ src/components/practice.js
@@ -1,41 +1,11 @@
-import { Component } from "react";
 
-class WhoAmI extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: '',
-      age: 28,
-      position: ''
-    };
-  }
-
-  changeName = (e) => {
-    this.setState({
-      name: e.target.value
-    })
-  }
-
-  commitInputChanges = (e) => {
-    this.setState({
-      position: e.target.value
-    });
-  }
-
-  render() {
-    const {name, age, position} = this.state;
-    return (
-      <div>
-        <h1>My name is {name}, age - {age}, position - {position}</h1>
-        <input type="text" onChange={this.changeName} />
-        <button onClick={this.changeAgeValue}>Click</button>
-        <form >
-          <span>Введите должность</span>
-          <input type="text" onChange={this.commitInputChanges} />
-        </form>
-      </div>
-    );
-  }
+function WhoAmI({ name, surname, email }) {
+  return (
+    <>
+      <h1>My name is {name}, surname - {surname}</h1>
+      My email: <a href="/">{email}</a>
+    </>
+  )
 }
 
 export default WhoAmI;
